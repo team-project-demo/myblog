755eadf9ee39fcbadcfa63b169ef0456ca89c702
teacher <itstep.codeclub@gmail.com>
2021-04-26 09:18:00 +0300

    AddComments

---
+++ MyBlog/TagHelpers/PageLinkTagHelper.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.Routing;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using MyBlog.ViewModels;
+
+namespace MyBlog.TagHelpers
+{
+    public class PageLinkTagHelper : TagHelper
+    {
+        private IUrlHelperFactory urlHelperFactory;
+
+        [ViewContext]
+        [HtmlAttributeNotBound]
+        public ViewContext ViewContext { get; set; }
+        public PageViewModel PageModel { get; set; }
+        public string PageAction { get; set; }
+
+        public PageLinkTagHelper(IUrlHelperFactory helperFactory)
+        {
+            urlHelperFactory = helperFactory;
+        }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            IUrlHelper urlHelper = urlHelperFactory.GetUrlHelper(ViewContext);
+            output.TagName = "div";
+
+            // набор ссылок будет представлять список ul
+            TagBuilder tag = new TagBuilder("ul");
+            tag.AddCssClass("pagination");
+
+            // формируем три ссылки - на текущую, предыдущую и следующую
+            TagBuilder currentItem = CreateTag(PageModel.PageNumber, urlHelper);
+
+            // создаем ссылку на предыдущую страницу, если она есть
+            if (PageModel.HasPreviousPage)
+            {
+                TagBuilder prevItem = CreateTag(PageModel.PageNumber - 1, urlHelper);
+                tag.InnerHtml.AppendHtml(prevItem);
+            }
+
+            tag.InnerHtml.AppendHtml(currentItem);
+
+            // создаем ссылку на следующую страницу, если она есть
+            if (PageModel.HasNextPage)
+            {
+                TagBuilder nextItem = CreateTag(PageModel.PageNumber + 1, urlHelper);
+                tag.InnerHtml.AppendHtml(nextItem);
+            }
+            output.Content.AppendHtml(tag);
+        }
+
+        TagBuilder CreateTag(int pageNumber, IUrlHelper urlHelper)
+        {
+            TagBuilder item = new("li");
+            TagBuilder link = new("a");
+            if (pageNumber == this.PageModel.PageNumber)
+            {
+                item.AddCssClass("active");
+            }
+            else
+            {
+                link.Attributes["href"] = urlHelper.Action(PageAction, new { pageNumber = pageNumber });
+            }
+            item.AddCssClass("page-item");
+            link.AddCssClass("page-link");
+            link.InnerHtml.Append(pageNumber.ToString());
+            item.InnerHtml.AppendHtml(link);
+            return item;
+        }
+    }
+}
