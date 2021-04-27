9625cc2189fc9363561879d37b37e6a82c95bf69
teacher <itstep.codeclub@gmail.com>
2021-04-27 13:16:22 +0300

    CreateComments

---
+++ MyBlog/Data/Migrations/20210427084932_CommentsUserFKProblem.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MyBlog.Data.Migrations
+{
+    public partial class CommentsUserFKProblem : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Comments_AspNetUsers_ApplicationUserId1",
+                table: "Comments");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Comments_ApplicationUserId1",
+                table: "Comments");
+
+            migrationBuilder.DropColumn(
+                name: "ApplicationUserId1",
+                table: "Comments");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ApplicationUserId",
+                table: "Comments",
+                type: "nvarchar(450)",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Comments_ApplicationUserId",
+                table: "Comments",
+                column: "ApplicationUserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Comments_AspNetUsers_ApplicationUserId",
+                table: "Comments",
+                column: "ApplicationUserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Comments_AspNetUsers_ApplicationUserId",
+                table: "Comments");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Comments_ApplicationUserId",
+                table: "Comments");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ApplicationUserId",
+                table: "Comments",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ApplicationUserId1",
+                table: "Comments",
+                type: "nvarchar(450)",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Comments_ApplicationUserId1",
+                table: "Comments",
+                column: "ApplicationUserId1");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Comments_AspNetUsers_ApplicationUserId1",
+                table: "Comments",
+                column: "ApplicationUserId1",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
--- MyBlog/Models/Comment.cs
+++ MyBlog/Models/Comment.cs
@@ -26,7 +26,7 @@ namespace MyBlog.Models
         [Display(Name = "Publish Time")]
         public DateTime PublishTime { get; set; }
 
-        public int ApplicationUserId { get; set; }
+        public string ApplicationUserId { get; set; }
         public virtual ApplicationUser ApplicationUser { get; set; }
 
         public int PostId { get; set; }
